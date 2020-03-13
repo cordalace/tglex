58530554e7840fef90b4b4db2da90e620a083f2e
Azat Kurbanov <cordalace@gmail.com>
2020-03-13 03:48:40 +0300

    Tokenize message entities

--- tests/test_tokenize.py
+++ tests/test_tokenize.py
@@ -1,24 +1,130 @@
+import dataclasses
 import datetime
+
+import pytest
 
 import tglex
 
+BASE_MESSAGE = tglex.Message(
+    message_id=42,
+    message_from=tglex.User(
+        id=43,
+        is_bot=False,
+        username='@user',
+    ),
+    chat=tglex.Chat(
+        id=44,
+        type=tglex.ChatType.GROUP,
+    ),
+    date=datetime.datetime.utcnow(),
+    text='Test text here',
+    entities=[],
+)
+
 
 def test_tokenize():
-    msg = tglex.Message(
-        message_id=42,
-        message_from=tglex.User(
-            id=43,
-            is_bot=False,
-            username='@user',
-        ),
-        chat=tglex.Chat(
-            id=44,
-            type=tglex.ChatType.GROUP,
-        ),
-        date=datetime.datetime.utcnow(),
-        text='Hello world',
-    )
-    assert tglex.tokenize(msg) == [
+    msg = dataclasses.replace(BASE_MESSAGE, text='Hello world')
+    assert list(tglex.tokenize(msg)) == [
         tglex.Token(text='Hello'),
         tglex.Token(text='world'),
+    ]
+
+
+@pytest.mark.parametrize('text,entity_type,expected_token', [
+    ('word', None, tglex.Token(text='word')),
+    (
+        '@user',
+        tglex.MessageEntityType.MENTION,
+        tglex.MentionToken(text='@user', user_id=None, username='@user'),
+    ),
+    (
+        '#пентон',
+        tglex.MessageEntityType.HASHTAG,
+        tglex.HashtagToken(text='#пентон'),
+    ),
+    (
+        '$USD',
+        tglex.MessageEntityType.CASHTAG,
+        tglex.CashtagToken(text='$USD'),
+    ),
+    (
+        '/start@jobs_bot',
+        tglex.MessageEntityType.BOT_COMMAND,
+        tglex.BotCommandToken(text='/start@jobs_bot'),
+    ),
+    (
+        'https://telegram.org',
+        tglex.MessageEntityType.URL,
+        tglex.URLToken(text='https://telegram.org'),
+    ),
+    (
+        'user@example.org',
+        tglex.MessageEntityType.EMAIL,
+        tglex.EmailToken(text='user@example.org'),
+    ),
+])
+def test_single_token(text, entity_type, expected_token):
+    """Test messages with a single token."""
+    if entity_type is None:
+        entities = []
+    else:
+        entities = [tglex.MessageEntity(
+            type=entity_type,
+            offset=0,
+            length=len(text),
+            user=None,
+        )]
+    msg = dataclasses.replace(BASE_MESSAGE, text=text, entities=entities)
+    assert list(tglex.tokenize(msg)) == [expected_token]
+
+
+def test_unknown_entity_type():
+    """Test skipping entities with unknown types."""
+    text = 'ok bad ok'
+    entities = [tglex.MessageEntity(
+        type=object(),
+        offset=3,  # offset of word 'bad'
+        length=len('bad'),
+        user=None,
+    )]
+    msg = dataclasses.replace(BASE_MESSAGE, entities=entities, text=text)
+    assert list(tglex.tokenize(msg)) == [
+        tglex.Token(text='ok'),
+        tglex.Token(text='ok'),
+    ]
+
+
+def test_entity_with_text_before():
+    """Test text when special entity preceded by usual text."""
+    usual_text = 'usual'
+    email_text = 'user@example.org'
+    text = '{} {}'.format(usual_text, email_text)
+    entities = [tglex.MessageEntity(
+        type=tglex.MessageEntityType.EMAIL,
+        offset=len(usual_text) + 1,
+        length=len(email_text),
+        user=None,
+    )]
+    msg = dataclasses.replace(BASE_MESSAGE, text=text, entities=entities)
+    assert list(tglex.tokenize(msg)) == [
+        tglex.Token(text=usual_text),
+        tglex.EmailToken(text=email_text),
+    ]
+
+
+def test_entity_with_text_after():
+    """Test text when special entity followed by usual text."""
+    email_text = 'user@example.org'
+    usual_text = 'usual'
+    text = '{} {}'.format(email_text, usual_text)
+    entities = [tglex.MessageEntity(
+        type=tglex.MessageEntityType.EMAIL,
+        offset=0,
+        length=len(email_text),
+        user=None,
+    )]
+    msg = dataclasses.replace(BASE_MESSAGE, text=text, entities=entities)
+    assert list(tglex.tokenize(msg)) == [
+        tglex.EmailToken(text=email_text),
+        tglex.Token(text=usual_text),
     ]
--- tglex/tokenize.py
+++ tglex/tokenize.py
@@ -1,11 +1,54 @@
 """Tokenize telegram messages."""
 
-from typing import List
+from typing import Generator, Optional, Tuple
 
 from tglex import message
 from tglex import token
 
 
-def tokenize(msg: message.Message) -> List[token.Token]:
+def split_entity_parts(
+        msg: message.Message,
+) -> Generator[Tuple[str, Optional[message.MessageEntity]], None, None]:
+    """Split message into parts according to entities."""
+    offset_shift = 0
+    text = msg.text
+    for i, entity in enumerate(sorted(msg.entities, key=lambda e: e.offset)):
+        start = entity.offset + offset_shift
+        end = start + entity.length
+        entity_text = text[start:end]
+
+        before, after = text[:start], text[end:]
+        text = after
+        offset_shift -= entity.offset + entity.length
+
+        if before:
+            yield before, None
+        yield entity_text, entity
+        if len(msg.entities) == i + 1 and after:  # last entity
+            yield after, None
+
+    if not msg.entities:
+        yield msg.text, None
+
+
+def tokenize(msg: message.Message) -> Generator[token.Token, None, None]:
     """Tokenize message."""
-    return [token.Token(text=x) for x in msg.text.split()]
+    for part_text, entity in split_entity_parts(msg):
+        if entity is None:
+            yield from (token.Token(text=x) for x in part_text.split())
+        elif entity.type == message.MessageEntityType.MENTION:
+            yield token.MentionToken(
+                text=part_text,
+                username=part_text,
+                user_id=None,
+            )
+        elif entity.type == message.MessageEntityType.HASHTAG:
+            yield token.HashtagToken(text=part_text)
+        elif entity.type == message.MessageEntityType.CASHTAG:
+            yield token.CashtagToken(text=part_text)
+        elif entity.type == message.MessageEntityType.BOT_COMMAND:
+            yield token.BotCommandToken(text=part_text)
+        elif entity.type == message.MessageEntityType.URL:
+            yield token.URLToken(text=part_text)
+        elif entity.type == message.MessageEntityType.EMAIL:
+            yield token.EmailToken(text=part_text)
